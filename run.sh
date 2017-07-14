d61c4cbd724369f2481e4f22a1b05440bcb3ad3c
Ilya Kreymer <ikreymer@gmail.com>
2017-07-14 16:11:28 +0000

    playbook: remove default wr_app_host and wr_content_host, ensure vars are defined (eg. via command line) in pre_tasks check run.sh: fix typos, exec as group only when needed, support --extra-vars nginx: move proxy_set_headers to each location block certbot: add renew crontab

--- run.sh
+++ run.sh
@@ -3,19 +3,27 @@ ansible --version
 
 if [ $? -ne 0 ]; then
     sudo apt-get update
-    sudo apt-get install -qqy python-pip libdev-ssl
+    sudo apt-get install -qqy python-pip libssl-dev
     sudo pip install cryptography ansible
     sudo ansible-galaxy install -r requirements.yml
 fi
 
 group=docker
+cmd="ansible-playbook -i hosts ./playbook.yml"
 
 if [ $(id -gn) != $group ]; then
   if ! grep -q $group /etc/group; then
     sudo groupadd docker
+  fi
+
+  if ! groups | grep -q $group; then
     sudo usermod -aG docker $USER
-    exec sg $group "$0 $*"
+    #exec sg $group "$0 $*"
+    exec sg $group "$cmd --extra-vars \"$1\""
+    return
   fi
 fi
 
-ansible-playbook -i hosts ./playbook.yml
+echo $cmd
+$cmd --extra-vars "$1"
+
