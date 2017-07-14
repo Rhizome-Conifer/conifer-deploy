17871b6e044ccb9deab5de4b4b03974349478c6b
Ilya Kreymer <ikreymer@gmail.com>
2017-07-14 03:52:45 +0000

    ansible work: - run.sh installs ansible via pip - run.sh adds docker group and runs with group settings applied - move wrhost and certbot to separate roles - using certbot --standalone for more flexible nginx settings

--- run.sh
+++ run.sh
@@ -1,1 +1,21 @@
+
+ansible --version
+
+if [ $? -ne 0 ]; then
+    sudo apt-get update
+    sudo apt-get install -qqy python-pip libdev-ssl
+    sudo pip install cryptography ansible
+    sudo ansible-galaxy install -r requirements.yml
+fi
+
+group=docker
+
+if [ $(id -gn) != $group ]; then
+  if ! grep -q $group /etc/group; then
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    exec sg $group "$0 $*"
+  fi
+fi
+
 ansible-playbook -i hosts ./playbook.yml
