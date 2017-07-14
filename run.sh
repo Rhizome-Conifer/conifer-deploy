6f4206db0da7c1b931e2a4bbf603809ccb31105f
Ilya Kreymer <ikreymer@gmail.com>
2017-07-14 10:47:01 -0700

    run: add make apt-get non-interactive, add comments, add 14.04 to readme

--- run.sh
+++ run.sh
@@ -2,15 +2,16 @@
 
 ansible --version
 
+# Install Ansible
 if [ $? -ne 0 ]; then
     sudo apt-get update
-    sudo apt-get install software-properties-common
+    sudo apt-get install -qqy software-properties-common
     sudo apt-add-repository ppa:ansible/ansible -y
     sudo apt-get update
-    sudo apt-get install ansible
+    sudo apt-get install -qqy ansible
 fi
 
-
+# Install Remote Roles
 if ! sudo ansible-galaxy list | grep -q 'nginx'; then
     sudo ansible-galaxy install -r requirements.yml
     sudo chown -R $USER ~/.ansible
@@ -19,6 +20,8 @@ fi
 group=docker
 cmd="ansible-playbook -i hosts ./playbook.yml"
 
+# If not in 'docker' group, need to switch to 
+# group to allow to docker ops as non-root user
 if [ $(id -gn) != $group ]; then
   if ! grep -q $group /etc/group; then
     sudo groupadd docker
